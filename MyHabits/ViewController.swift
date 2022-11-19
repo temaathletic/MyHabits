c0bb8e1548e715ee68da7c94add5602efb686ff0
Artem Zaytsev <101665702+temaathletic@users.noreply.github.com>
2022-11-19 20:44:10 +0300

    Initial Commit

---
+++ MyHabits/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyHabits
+//
+//  Created by temaathletic on 19.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
